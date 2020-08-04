1b0cd4f4c860573ea4d8080a790de2400086cd56
Mateus <mateuspsilva.dev@gmail.com>
2020-08-04 15:53:59 -0300

    Install sqlite3 dependecies and migrations for bd

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,22 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
-app.get('/users'), () => {
-    console.log('Acessou a rota');
-};
+// Para o express entender o formato json
+app.use(express.json());
 
-app.listen(3333);
+// http://localhost:3333/users
+// /users - é um resource!!!
+
+// Parametros
+// body -> criação ou atualização da informação
+// Route params => identificar um recurso. (Atualizar/deletar)
+// Query params -> paginação, outras informações de pesquisa basicamente
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Helloooooo!' });
+});
+
+app.listen(3333, () => {
+    console.log('🐺 Server running.')
+});
