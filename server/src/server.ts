fcafbb8fc658fc9c8be02cf0c4c8169bff58a375
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2020-08-06 00:14:19 -0300

    Completed the API

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
-import express, { response } from 'express';
+import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
 // Para o express entender o formato json
 app.use(express.json());
+// Para que outros endereços possam acessar a api
+app.use(cors());
 
 // http://localhost:3333/users
 // /users - é um resource!!!
@@ -14,6 +17,7 @@ app.use(express.json());
 // Route params => identificar um recurso. (Atualizar/deletar)
 // Query params -> paginação, outras informações de pesquisa basicamente
 
+// Usar o arquivo das rotas
 app.use(routes);
 
 app.listen(3333, () => {
